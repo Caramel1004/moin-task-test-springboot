e3c1ee3392d6f92941300d8da1db6060ce2c3ca4
Caramel1004 <Caramel1004.js@gmail.com>
2024-02-24 19:27:32 +0900

    record클래스로 변경, @Builder추가: 코드 간결해짐, 유동적으로 데이터를 세팅 할 수 있음

--- src/main/java/com/moin/remittance/domain/dto/responsebody/HttpResponseBody.java
+++ src/main/java/com/moin/remittance/domain/dto/responsebody/HttpResponseBody.java
@@ -1,10 +1,6 @@
 package com.moin.remittance.domain.dto.responsebody;
 
-import com.moin.remittance.domain.vo.HttpResponseCode;
 import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import org.springframework.http.ResponseEntity;
 
 @Builder
 public record HttpResponseBody<T>(int statusCode, String message, String codeName, T data) {
