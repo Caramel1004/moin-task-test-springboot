dce5b06f96b9f2985f77a22a6ccfb02a02076474
Caramel1004 <Caramel1004.js@gmail.com>
2024-02-25 16:37:59 +0900

    jpa 경로 수정

--- src/main/java/com/moin/remittance/OverseasRemittanceApplication.java
+++ src/main/java/com/moin/remittance/OverseasRemittanceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.moin.remittance.domain.repository")
+@EnableJpaRepositories(basePackages = "com.moin.remittance.repository")
 @SpringBootApplication(scanBasePackages = "com.moin.remittance.*")
 public class OverseasRemittanceApplication {
 
