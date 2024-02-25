95bc109a0cd1d426daa11f42370deb63c5214e5c
Caramel1004 <Caramel1004.js@gmail.com>
2024-02-25 11:36:13 +0900

    문자열 타입 변수 codeName 추가

--- src/main/java/com/moin/remittance/exception/dto/ErrorResponseDTO.java
+++ src/main/java/com/moin/remittance/exception/dto/ErrorResponseDTO.java
@@ -1,32 +1,30 @@
 package com.moin.remittance.exception.dto;
 
-import com.moin.remittance.core.configration.exception.*;
-import com.moin.remittance.core.exception.*;
-import com.moin.remittance.domain.exception.*;
-import com.moin.remittance.domain.vo.HttpResponseStatusVO;
+
+import com.moin.remittance.domain.vo.HttpResponseCode;
 import com.moin.remittance.exception.*;
 import lombok.Data;
 
 @Data
 public class ErrorResponseDTO {
-    private HttpResponseStatusVO codeName;
+    private String codeName;
     private int code;
     private String message;
 
     public ErrorResponseDTO(NegativeNumberException e) {
-        this.codeName = e.getErrorCode();
+        this.codeName = e.getCodeName();
         this.code = e.getCode();
         this.message = e.getMessage();
     }
 
     public ErrorResponseDTO(ExternalAPIException e) {
-        this.codeName = e.getErrorCode();
+        this.codeName = e.getCodeName();
         this.code = e.getCode();
         this.message = e.getMessage();
     }
 
     public ErrorResponseDTO(NotExternalDataException e) {
-        this.codeName = e.getErrorCode();
+        this.codeName = e.getCodeName();
         this.code = e.getCode();
         this.message = e.getMessage();
 
@@ -34,38 +32,38 @@ public class ErrorResponseDTO {
     }
 
     public ErrorResponseDTO(ExpirationTimeOverException e) {
-        this.codeName = e.getErrorCode();
+        this.codeName = e.getCodeName();
         this.code = e.getCode();
         this.message = e.getMessage();
     }
 
     public ErrorResponseDTO(AmountLimitExcessException e) {
-        this.codeName = e.getErrorCode();
-        this.code = codeName.getCode();
-        this.message = codeName.getMessage();
+        this.codeName = e.getCodeName();
+        this.code = e.getCode();
+        this.message = e.getMessage();
     }
 
     public ErrorResponseDTO(InValidPatternTypeException e) {
-        this.codeName = e.getErrorCode();
-        this.code = codeName.getCode();
-        this.message = codeName.getMessage();
+        this.codeName = e.getCodeName();
+        this.code = e.getCode();
+        this.message = e.getMessage();
     }
 
     public ErrorResponseDTO(DuplicateUserIdException e) {
-        this.codeName = e.getErrorCode();
-        this.code = codeName.getCode();
-        this.message = codeName.getMessage();
+        this.codeName = e.getCodeName();
+        this.code = e.getCode();
+        this.message = e.getMessage();
     }
 
     public ErrorResponseDTO(NotFoundMemberException e) {
-        this.codeName = e.getErrorCode();
-        this.code = codeName.getCode();
-        this.message = codeName.getMessage();
+        this.codeName = e.getCodeName();
+        this.code = e.getCode();
+        this.message = e.getMessage();
     }
-    public ErrorResponseDTO(HttpResponseStatusVO codeName) {
-        this.codeName = codeName;
-        this.code = codeName.getCode();
-        this.message = codeName.getMessage();
+    public ErrorResponseDTO(HttpResponseCode errorCode) {
+        this.codeName = errorCode.getCodeName();
+        this.code = errorCode.getStatusCode();
+        this.message = errorCode.getMessage();
     }
 
 
