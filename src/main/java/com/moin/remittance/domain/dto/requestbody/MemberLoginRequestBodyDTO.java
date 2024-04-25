62f3d41d5c74e89e7d9f42bd92a047bf67927b70
Caramel1004 <Caramel1004.js@gmail.com>
2024-04-25 10:45:51 +0900

    ♻️ [refactor] 서블릿 필터 변수명에 맞게 변경: userId => username
    ♻️ [refactor] 서블릿 필터 변수명에 맞게 변경: userId => username

--- src/main/java/com/moin/remittance/domain/dto/requestbody/MemberLoginRequestBodyDTO.java
+++ src/main/java/com/moin/remittance/domain/dto/requestbody/MemberLoginRequestBodyDTO.java
@@ -10,7 +10,7 @@ import lombok.Data;
 public class MemberLoginRequestBodyDTO {
     @NotBlank(message = "이메일을 입력해주세요.")
     @Email(message = "이메일 형식으로 입력해주세요.")
-    private String userId;
+    private String username;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
