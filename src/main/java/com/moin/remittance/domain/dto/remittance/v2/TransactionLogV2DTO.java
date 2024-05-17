55c2207814ce2d67e08cf62a4de30e8957deb104
Caramel1004 <Caramel1004.js@gmail.com>
2024-05-17 09:28:28 +0900

    ♻️ [refactor] Test data 모니터링을 위한 toString메소드 오버라이딩
    ♻️  [refactor] Test data 모니터링을 위한 toString메소드 오버라이딩

--- src/main/java/com/moin/remittance/domain/dto/remittance/v2/TransactionLogV2DTO.java
+++ src/main/java/com/moin/remittance/domain/dto/remittance/v2/TransactionLogV2DTO.java
@@ -1,10 +1,10 @@
 package com.moin.remittance.domain.dto.remittance.v2;
 
+
 import lombok.Builder;
 import lombok.Getter;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
 
 @Getter
@@ -15,4 +15,15 @@ public class TransactionLogV2DTO {
     private int todayTransferCount;
     private BigDecimal todayTransferUsdAmount;
     private List<RemittanceHistoryV2DTO> history;
+
+    @Override
+    public String toString() {
+        return "RemittanceLogEntityV2 {" + "\n" +
+                "\tuserId: " + userId + ",\n" +
+                "\tname: " + name + ",\n" +
+                "\ttodayTransferCount: " + todayTransferCount + ",\n" +
+                "\ttodayTransferUsdAmount: " + todayTransferUsdAmount + ",\n" +
+                "\thistory: \n" + history + "\n" +
+                '}';
+    }
 }
