acfcc06ff430c123d8ae810fca9e958469293257
Caramel1004 <Caramel1004.js@gmail.com>
2024-05-17 09:31:36 +0900

    ♻️ [refactor] Test용 쿼리메소드 추가
    ♻️  [refactor] Test용 쿼리메소드 추가

--- src/main/java/com/moin/remittance/repository/v2/MemberRepositoryV2.java
+++ src/main/java/com/moin/remittance/repository/v2/MemberRepositoryV2.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.lang.reflect.Member;
 import java.util.UUID;
 
 @Repository
