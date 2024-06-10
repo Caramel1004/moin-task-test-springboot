f13570c629d5bfb62ec2df5d39c317ff69a145bd
Caramel1004 <Caramel1004.js@gmail.com>
2024-06-10 12:51:19 +0900

    ✅ [TEST] 두나무 환율 정보 API 호출 테스트
    [TEST] 두나무 환율 정보 API 호출 테스트

--- src/test/java/com/moin/remittance/application/api/ExchangeRateApiClientImplV2Test.java
+++ src/test/java/com/moin/remittance/application/api/ExchangeRateApiClientImplV2Test.java
@@ -12,15 +12,9 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.HashMap;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(MockitoExtension.class)
 public class ExchangeRateApiClientImplV2Test {
@@ -28,49 +22,38 @@ public class ExchangeRateApiClientImplV2Test {
     @InjectMocks
     private ExchangeRateApiClientImplV2 webClientServiceMock;
 
-    @Mock
-    private WebClient webClientMock;
-
-    private List<ExchangeRateInfoDTO> testCase;
 
     @Test
-    @DisplayName("WebFlux 로 외부 API 호출")
-    void fetchExApiTest() {
-        String codes = "FRX.KRWUSD";
-
-        /* given: 외부 API 요청 */
-        WebClient webClient =
-                WebClient
-                        .builder()
-                        .baseUrl("https://quotation-api-cdn.dunamu.com")
-                        .build();
-
-        List<ExchangeRateInfoDTO> exchangeRateInfoList = webClient.get()
-                .uri(builder -> builder
-                        .path("/v1/forex/recent")
-                        .queryParam("codes", codes)
-                        .build()
-                )
-                .exchangeToMono(res -> {
-                    return res.bodyToMono(new ParameterizedTypeReference<List<ExchangeRateInfoDTO>>() {
-                    });
-                })
-                .block();// 배열 json 으로 넘어온 데이터를 List 로 변환
-
-        System.out.println(exchangeRateInfoList);
-
-        assertNotNull(exchangeRateInfoList);
-        assertFalse(exchangeRateInfoList.isEmpty());
-    }
-
-    @Test
-    @DisplayName("두나무 외부 API로 환율 정보 호출")
+    @DisplayName("WebFlux 테스트: 두나무 환율 정보 API 요청")
     void getExchangeRateInfoTest() {
 
-        /* given */
-        HashMap<String, ExchangeRateInfoDTO> hashMap = webClientServiceMock.fetchExchangeRateInfoFromExternalAPI("FRX.KRWUSD");
+        /* when: 외부 API 호출
+        *  codes: USD
+        * */
+        HashMap<String, ExchangeRateInfoDTO> map1 =
+                webClientServiceMock.fetchExchangeRateInfoFromExternalAPI("FRX.KRWUSD");
 
-        System.out.println("hashMap: " + hashMap);
+        /* when: 외부 API 호출
+        *  codes: USD, JPY, EUR
+        * */
+        HashMap<String, ExchangeRateInfoDTO> map2 =
+                webClientServiceMock.fetchExchangeRateInfoFromExternalAPI("FRX.KRWUSD,FRX.KRWJPY,FRX.KRWEUR");
+
+
+        System.out.println("map1: " + map1);
+        System.out.println("map2: " + map2);
+
+        assertNotNull(map1);
+        assertNotNull(map2);
+
+        assertNotNull(map1.get("USD"));
+
+        assertNotNull(map2.get("USD"));
+        assertNotNull(map2.get("JPY"));
+        assertNotNull(map2.get("EUR"));
+
+        assertFalse(map1.isEmpty());
+        assertFalse(map2.isEmpty());
     }
 
 }
